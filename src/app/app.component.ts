8bf2a65cf49d2924c63888bbabf992c31636b188
basherMX <brayanvazq1608@gmail.com>
2022-11-29 22:31:13 -0600

    create form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+// import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ProyectoRedes';
+export class AppComponent /* implements AfterViewInit */{
+  // private map;
+
+  // private initMap(): void {
+  //   this.map = L.map('map', {
+  //     center: [ 39.8282, -98.5795 ],
+  //     zoom: 3
+  //   });
+  // }
+
+
+
+  // ngAfterViewInit(): void {
+  //   this.initMap();
+  // }
+
+
+
 }
